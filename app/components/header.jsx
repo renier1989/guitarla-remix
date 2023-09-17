fe19d1d1fe911dd39b34a40a759180518060f7fc
Renier Vargas <renier1989@gmail.com>
2023-09-17 17:51:38 -0500

    Class199. Resaltar la página actual

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,6 +1,9 @@
-import { Link } from "@remix-run/react"
+import { Link, useLocation } from "@remix-run/react"
 import logo from "../../public/img/logo.svg"
 function Header() {
+    const location = useLocation();
+    console.log("🚀 ~ file: header.jsx:5 ~ Header ~ location:", location)
+    
   return (
     <header className="header">
         <div className="contenedor barra">
@@ -10,10 +13,10 @@ function Header() {
             </Link>
         </div>
         <nav className="navegacion">
-            <Link to="/">Inicio</Link>
-            <Link to="/nosotros">Nosotros</Link>
-            <Link to="/tienda">Tienda</Link>
-            <Link to="/blog">Blog</Link>
+            <Link to="/" className={location.pathname === '/' ? 'active': ''}>Inicio</Link>
+            <Link to="/nosotros"className={location.pathname === '/nosotros' ? 'active': ''}>Nosotros</Link>
+            <Link to="/tienda"className={location.pathname === '/tienda' ? 'active': ''}>Tienda</Link>
+            <Link to="/blog"className={location.pathname === '/blog' ? 'active': ''}>Blog</Link>
         </nav>
         </div>
     </header>
