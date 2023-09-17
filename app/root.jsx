49afe998ad6d1c1724187a10f893a8e280134624
Renier Vargas <renier1989@gmail.com>
2023-09-17 10:48:00 -0500

    Class191. Añadir Información Meta

--- app/root.jsx
+++ app/root.jsx
@@ -1,3 +1,17 @@
+import { Meta } from "@remix-run/react";
+
+export const meta = () => {
+  return [
+    {
+      viewport: "width=device-width,initial-scale=1",
+    },
+    { title: "New Remix App" },
+    {
+      charset: "charset",
+    },
+  ];
+};
+
 export default function App() {
   return <Document>Desde la app</Document>;
 }
@@ -5,7 +19,9 @@ export default function App() {
 function Document({ children }) {
   return (
     <html lang="es">
-      <title>GuitarLA - Remix</title>
+      <head>
+        <Meta />
+      </head>
       <body>{children}</body>
     </html>
   );
