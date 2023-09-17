26d53f2b188ae6cfa9c4a41545eca56451c9f6d9
Renier Vargas <renier1989@gmail.com>
2023-09-17 18:13:28 -0500

    Class201. CSS al Footer

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -2,11 +2,13 @@ import Navigation from "./navigation";
 
 function Footer() {
   return (
-    <footer>
-        <Navigation/>
-        <p>Todos los derechos reservados {new Date().getFullYear()}</p>
+    <footer className="footer">
+      <div className="contenedor contenido">
+        <Navigation />
+        <p className="copyright">Todos los derechos reservados {new Date().getFullYear()}</p>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
