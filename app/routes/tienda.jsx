c678473c0059c30afbb1230502bfc39b2c01a2e9
Renier Vargas <renier1989@gmail.com>
2023-09-22 06:16:52 -0500

    Class204. Consumiendo el Backend de Strapi con un loader

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,3 +1,11 @@
+// $2a$10$klP5caXmg/60/SJN3c8AuOjLzAD0jLJzI9xcD4vgTD7K87jLjQOJO
+
+export async function loader(){
+  const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`);
+  const resultado = await respuesta.json();
+  console.log(process.env.API_URL , resultado);
+  return {}
+}
 
 function Tienda() {
   return (
