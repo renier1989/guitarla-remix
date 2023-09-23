10bcc7922f387ef3054eda88bee66e8b86ce0d5e
Renier Vargas <renier1989@gmail.com>
2023-09-22 19:32:38 -0500

    Class206. Obtener las guitarras y llamarlas en el componente con useLoaderData

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,15 +1,30 @@
-import { getGuitarras } from '~/models/guitarras.server';
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarras } from "~/models/guitarras.server";
+import Guitarra from "~/components/guitarra";
 
-export async function loader(){
+export async function loader() {
   const guitarras = await getGuitarras();
-  console.log(guitarras);
-  return {}
+  return guitarras.data;
 }
 
 function Tienda() {
+  const guitarras = useLoaderData();
   return (
-    <div>Tienda</div>
-  )
+    <main className="contenedor">
+      <h2 className="heading"> Nuestra Colección</h2>
+
+      {guitarras?.length && (
+        <div className="guitarras-grid">
+          {guitarras.map((guitarra) => (
+            <Guitarra 
+              key={guitarra?.id}
+              guitarra={guitarra?.attributes} 
+            />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
 
-export default Tienda
+export default Tienda;
