552a3ba997e08f9b30f5699c8ec61687edce7493
Renier Vargas <renier1989@gmail.com>
2023-09-22 21:17:44 -0500

    Class212. Añadir Información Meta dinamicamente

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,17 +1,30 @@
-import { useLoaderData } from '@remix-run/react';
-import { getGuitarra } from '~/models/guitarras.server'
-import styles from "~/styles/guitarras.css"
-export function links(){
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarra } from "~/models/guitarras.server";
+import styles from "~/styles/guitarras.css";
+
+export function meta({data}){
+  return[
+    {
+      title: `GuitarLa - ${data.data[0].attributes.nombre}`,
+    },
+    {
+      name:'description',
+      content: `GuitarLa - Venta de guitarras de nuestra mejor coleccion ${data.data[0].attributes.nombre}`
+    }
+  ]
+}
+
+export function links() {
   return [
     {
       rel: "stylesheet",
       href: styles,
     },
-  ]
+  ];
 }
 
-export async function loader({params}){
-  const {guitarraUrl} = params;
+export async function loader({ params }) {
+  const { guitarraUrl } = params;
   const guitarra = await getGuitarra(guitarraUrl);
   // console.log(guitarra);
   return guitarra;
@@ -19,19 +32,22 @@ export async function loader({params}){
 
 function Guitarra() {
   const guitarra = useLoaderData();
-  const {nombre, descripcion, imagen , precio} = guitarra.data[0].attributes;
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
   console.log(guitarra);
   return (
-    <main className='contenedor guitarra'>
-      <img className='imagen' src={imagen.data.attributes.url} alt={`guitarra ${nombre}`} />
-      <div className='contenido'>
+    <main className="contenedor guitarra">
+      <img
+        className="imagen"
+        src={imagen.data.attributes.url}
+        alt={`guitarra ${nombre}`}
+      />
+      <div className="contenido">
         <h3>{nombre}</h3>
-        <p className='texto'>{descripcion}</p>
-        <p className='precio'>${precio}</p>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio}</p>
       </div>
-      
     </main>
-  )
+  );
 }
 
-export default Guitarra
+export default Guitarra;
