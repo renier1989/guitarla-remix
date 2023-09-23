1f9e842578aa23620a6db326005e146946b1a174
Renier Vargas <renier1989@gmail.com>
2023-09-22 21:10:28 -0500

    Class211. Mostrando la Información de la guitarra

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,5 +1,14 @@
 import { useLoaderData } from '@remix-run/react';
 import { getGuitarra } from '~/models/guitarras.server'
+import styles from "~/styles/guitarras.css"
+export function links(){
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ]
+}
 
 export async function loader({params}){
   const {guitarraUrl} = params;
@@ -10,9 +19,18 @@ export async function loader({params}){
 
 function Guitarra() {
   const guitarra = useLoaderData();
+  const {nombre, descripcion, imagen , precio} = guitarra.data[0].attributes;
   console.log(guitarra);
   return (
-    <div>GuitarraUrl</div>
+    <main className='contenedor guitarra'>
+      <img className='imagen' src={imagen.data.attributes.url} alt={`guitarra ${nombre}`} />
+      <div className='contenido'>
+        <h3>{nombre}</h3>
+        <p className='texto'>{descripcion}</p>
+        <p className='precio'>${precio}</p>
+      </div>
+      
+    </main>
   )
 }
 
