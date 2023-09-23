dc590fbfb62043acbbf468780a57bb06231031a1
Renier Vargas <renier1989@gmail.com>
2023-09-22 19:42:11 -0500

    Class207. Mostrando la información de la guitarra

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -1,9 +1,20 @@
+import { Link } from "@remix-run/react";
 
-function Guitarra({guitarra}) {
-    console.log(guitarra);
+function Guitarra({ guitarra }) {
+  const { descripcion, imagen, precio, url, nombre } = guitarra;
+
   return (
-    <div>Guitarra</div>
-  )
+    <div className="guitarra">
+        <img src={imagen.data.attributes.formats.medium.url} alt={`imagen guitarra ${nombre}`} />
+      <div className="contenido">
+        <h3>{nombre}</h3>
+        <p className="descripcion">{descripcion}</p>
+        <p className="precio">${precio}</p>
+
+        <Link to={`/guitarras/${url}`}>enlace</Link>
+      </div>
+    </div>
+  );
 }
 
-export default Guitarra
+export default Guitarra;
