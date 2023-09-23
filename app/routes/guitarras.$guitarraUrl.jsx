b8e49fd11a7c8ea82c7067c39c401a5c12503df7
Renier Vargas <renier1989@gmail.com>
2023-09-22 21:00:42 -0500

    ClassFiltrar Guitarras por URL desde Strapi

--- app/routes/guitarras.$guitarraUrl.jsx
+++ app/routes/guitarras.$guitarraUrl.jsx
@@ -1,10 +1,16 @@
+import { useLoaderData } from '@remix-run/react';
+import { getGuitarra } from '~/models/guitarras.server'
 
-export function loader({params}){
+export async function loader({params}){
   const {guitarraUrl} = params;
-  return {};
+  const guitarra = await getGuitarra(guitarraUrl);
+  // console.log(guitarra);
+  return guitarra;
 }
 
 function Guitarra() {
+  const guitarra = useLoaderData();
+  console.log(guitarra);
   return (
     <div>GuitarraUrl</div>
   )
