17451c45aca4d8704d374d66db49b262598377d0
Renier Vargas <renier1989@gmail.com>
2023-09-23 13:27:11 -0500

    Class217 - 218 Routind dinamico para el bloc y ajustes finales

--- app/models/posts.server.js
+++ app/models/posts.server.js
@@ -2,3 +2,8 @@ export async function getPosts(){
     const res = await fetch(`${process.env.API_URL}/posts?populate=imagen`)
     return await res.json();
 }
+
+export async function getPost(url){
+    const respuesta = await fetch(`${process.env.API_URL}/posts?filters[url]=${url}&populate=imagen`)
+    return await respuesta.json();
+}
---
+++ app/routes/posts.$postUrl.jsx
@@ -0,0 +1,68 @@
+import { useLoaderData } from "react-router";
+import {getPost} from "../models/posts.server"
+import {formatearFecha} from "../utils/helpers"
+import styles from "../styles/blogs.css"
+
+export async function loader({params}){
+    const {postUrl} = params;
+    const post = await getPost(postUrl);
+    
+    if(post.data.length === 0){
+        throw new Response('',{
+            status: 404,
+            statusText: 'Post no Encontrado'
+        })
+    }
+    return post?.data[0]?.attributes;
+}
+
+export function meta({data}){
+
+    
+    if(!data){
+        return[
+            {
+                title: `GuitarLa - Blog no encontrado`,
+            },
+            {
+                name: 'description',
+                content: `GuitarLa - contenido de este blog de tiendas de guitarras no encontrado`
+            }
+        ]
+    }
+
+    const {titulo} =  data;
+    
+    return[
+        {
+            title: `GuitarLa - Blog ${titulo}`,
+        },
+        {
+            name: 'description',
+            content: `GuitarLa - contenido de este blog de tiendas de guitarras ${titulo}`
+        }
+    ]
+}
+
+export function links(){
+    return[
+        {
+            rel: "stylesheet",
+            href : styles
+        }
+    ]
+}
+
+export default function Post() {
+    const post = useLoaderData();
+    const {contenido, imagen, titulo, publishedAt} = post
+    
+  return (
+    <main className="post contenedor mt-3">
+        <img src={imagen?.data?.attributes?.url} alt={`Imagen del post ${titulo}`} />
+        <h3 className="heading">{titulo}</h3>
+        <p className="fecha">{formatearFecha(publishedAt)}</p>
+        <p className="texto">{contenido}</p>
+    </main>
+  )
+}
