1ae62c16238da2f80736eefb40d8bfd94bd53f66
Renier Vargas <renier1989@gmail.com>
2023-09-25 06:31:21 -0500

    Class221. Llamar el Contenido para el Curso y aplicar CSS con jsx

--- app/routes/_index.jsx
+++ app/routes/_index.jsx
@@ -1,10 +1,13 @@
 import { useLoaderData } from "@remix-run/react";
 import { getGuitarras } from "../models/guitarras.server";
+import { getCurso } from "../models/curso.server";
 import { getPosts } from "../models/posts.server";
 import ListadoGuitarras from "../components/listado-guitarras";
 import stylesGuitarras from "../styles/guitarras.css";
+import stylesCurso from "../styles/curso.css";
 import ListadoPosts from "../components/listado-posts";
 import stylesPosts from "../styles/blogs.css";
+import Curso from "../components/curso";
 
 export function links() {
   return [
@@ -16,6 +19,10 @@ export function links() {
       rel: "stylesheet",
       href: stylesPosts,
     },
+    {
+      rel: "stylesheet",
+      href: stylesCurso,
+    },
   ];
 }
 
@@ -23,10 +30,15 @@ export function meta() {}
 
 export async function loader() {
   // esta es una opcion mas optimizada para llamar multiples apis , estas se ejecutan en simultaneo
-  const [guitarras, posts] = await Promise.all([getGuitarras(), getPosts()]);
+  const [guitarras, posts, curso] = await Promise.all([
+    getGuitarras(),
+    getPosts(),
+    getCurso(),
+  ]);
 
-  console.log(guitarras);
-  console.log(posts);
+  // console.log(guitarras);
+  // console.log(posts);
+  // console.log(curso);
 
   // esta seria la forma comun de llamar a dos apis, pero esta espera a que la primera termine de cargar luego la segunda carga los datos.
   // const guitarras = await getGuitarras();
@@ -37,20 +49,23 @@ export async function loader() {
   return {
     guitarras: guitarras?.data,
     posts: posts?.data,
+    curso: curso?.data,
   };
 }
 
 function Index() {
-  const { guitarras, posts } = useLoaderData();
+  const { guitarras, posts, curso } = useLoaderData();
   return (
     <>
       <main className="contenedor">
         <ListadoGuitarras guitarras={guitarras} />
-
-        <section className="contenedor">
-          <ListadoPosts posts={posts} />
-        </section>
       </main>
+
+      <Curso curso={curso.attributes} />
+
+      <section className="contenedor">
+        <ListadoPosts posts={posts} />
+      </section>
     </>
   );
 }
