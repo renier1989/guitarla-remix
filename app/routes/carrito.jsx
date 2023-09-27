a45384d07f242b840f825d5d2a0f6ed423f9b6b8
Renier Vargas <renier1989@gmail.com>
2023-09-27 06:21:07 -0500

    Class225. Añadiendo el Contenido y CSS

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,7 +1,34 @@
-// import React from 'react'
+import styles from "../styles/carrito.css";
+
+export function meta() {
+  return [
+    { title: "GuitarLA - Carrito" },
+    { name: "description", content: "GuitarLA - Carrito de compra" },
+  ];
+}
+
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
 
 export default function Carrito() {
   return (
-    <div>Carrito</div>
-  )
+    <main className="contenedor">
+      <h1 className="heading">Carrito de compras</h1>
+      <div className="contenido">
+        <div className="carrito">
+          <h2>Articulos</h2>
+        </div>
+        <aside className="resumen">
+          <h3>Resumen del pedido</h3>
+          <p>Total a pagar: $999</p>
+        </aside>
+      </div>
+    </main>
+  );
 }
