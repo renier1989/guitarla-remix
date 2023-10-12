269948ff8cc86c9ae9b861c8b3b3b8abb31b12c0
Renier Vargas <renier1989@gmail.com>
2023-10-11 21:05:37 -0500

    Class234. CSS a los elementos del Carrito

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -18,7 +18,7 @@ export function links() {
 }
 
 export default function Carrito() {
-  const {carrito} = useOutletContext();
+  const { carrito } = useOutletContext();
   console.log(carrito);
   return (
     <main className="contenedor">
@@ -27,22 +27,26 @@ export default function Carrito() {
         <div className="carrito">
           <h2>Articulos</h2>
 
-          {carrito.length === 0 ? 'Carrito vacio' : (
-            carrito.map(producto => (
-              <div key={producto.id}>
-                <div>
-                  <img src={producto.imagen} alt={`imagen de la guitarra ${producto.nombre}`} />
-                </div>
-                <div>
+          {carrito.length === 0
+            ? "Carrito vacio"
+            : carrito.map((producto) => (
+                <div key={producto.id} className="producto">
+                  <div>
+                    <img
+                      src={producto.imagen}
+                      alt={`imagen de la guitarra ${producto.nombre}`}
+                    />
+                  </div>
+                  <div>
                     <p className="nombre">{producto.nombre}</p>
-
+                    <p className="cantidad"> Cantidad: {producto.cantidad}</p>
                     <p className="precio">${producto.precio}</p>
-                    <p className="subtotal">Subtotal: ${producto.cantidad * producto.precio}</p>
+                    <p className="subtotal">
+                      Subtotal: ${producto.cantidad * producto.precio}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            ))
-          )}
-
+              ))}
         </div>
         <aside className="resumen">
           <h3>Resumen del pedido</h3>
