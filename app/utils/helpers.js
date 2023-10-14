18ec27cd4250119bf88abd3398701b5071c86076
Renier Vargas <renier1989@gmail.com>
2023-10-13 19:59:07 -0500

    cambios

--- app/utils/helpers.js
+++ app/utils/helpers.js
@@ -1,9 +1,9 @@
-// export const formatearFecha = (fecha) => {
-//   const fechaNueva = new Date(fecha);
-//   const opciones = {
-//     year: "numeric",
-//     month: "long",
-//     day: "2-digit",
-//   };
-//   return fechaNueva.toLocaleDateString("es-ES", opciones);
-// };
+export const formatearFecha = (fecha) => {
+  const fechaNueva = new Date(fecha);
+  const opciones = {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  };
+  return fechaNueva.toLocaleDateString("es-ES", opciones);
+};
